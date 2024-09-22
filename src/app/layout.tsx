3b28393a56045c8667533637031201286ab20eae
CJM <whdals9514@naver.com>
2024-09-22 18:48:36 +0900

    :hammer: Metadata fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,32 +8,10 @@ import { Footer } from "@/components/layout/Footer";
 import "./globals.css";
 import { Metadata } from "next";
 
-// const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
-
 export const metadata: Metadata = {
-  // metadataBase: new URL(siteUrl),
   title: "공작기계 솔루션 비더원",
   description: "가장 쉽고 합리적인 제조 솔루션 공작기계 부착형 무선 데이터 서버, 캐드, 캠, 스마트팩토리, 모니터링 서비스를 소개합니다.",
   keywords: "제조 솔루션, 부착형 무선 데이터 서버, 화낙, LAN, RS232, 캐드, 캠, 스마트팩토리, 모니터링",
-  // openGraph: {
-  //   title: '공작기계 솔루션 비더원',
-  //   description: '가장 쉽고 합리적인 제조 솔루션 비더원',
-  //   url: "/",
-  //   images: [
-  //     {
-  //       url: '/Logo.png',
-  //       width: 1200,
-  //       height: 630,
-  //       alt: '비더원 로고'
-  //     }
-  //   ]
-  // },
-  // twitter: {
-  //   card: 'summary_large_image',
-  //   title: '공작기계 솔루션 비더원',
-  //   description: '가장 쉽고 합리적인 제조 솔루션 비더원',
-  //   images: ['/Logo.png']
-  // }
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,31 @@ import { Main_4 } from "@/components/section/main/Main_4";
 import UserCarousel from "@/app/mainComponents/UserCarousel";
 import type { Metadata } from 'next'
 
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 export const metadata: Metadata = {
-  title: '공작기계 솔루션 비더원',
-  description: "가장 쉽고 합리적인 제조 솔루션 공작기계 부착형 무선 데이터 서버, 캐드, 캠, 스마트팩토리, 모니터링 서비스를 소개합니다.",
-  keywords: "제조 솔루션, 부착형 무선 데이터 서버, 무선 데이터 서버, 데이터 서버, 화낙, LAN, RS232, 캐드, 캠, 스마트팩토리, 모니터링",
+  metadataBase: new URL(siteUrl),
+  title: '부착형 무선 데이터 서버',
+  description: "제조 인프라의 혁신 부착형 무선 데이터 서버를 소개합니다.",
+  keywords: "가격, 가격확인, 부착형 무선 데이터 서버, 무선 데이터 서버, 데이터 서버, 화낙, LAN, RS232, 스마트팩토리, 모니터링",
+  openGraph: {
+    title: '공작기계 솔루션 비더원',
+    description: '가장 쉽고 빠른 부착형 데이터 서버',
+    url: siteUrl,
+    images: [
+      {
+        url: '/Logo.png',
+        width: 800,
+        height: 600,
+        alt: 'Betheone Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '공작기계 솔루션 비더원',
+    description: '가장 쉽고 빠른 부착형 데이터 서버',
+    images: ['/Logo.png'],
+  },
 }
 
 export default function Home() {
