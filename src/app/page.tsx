ab258095d29857c7bce8c869745a1e4bf1904bb4
cjm-korea <whdals9514@naver.com>
2024-09-24 21:16:45 +0900

    :hammer: OG url fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Logopath from "@/public/Logo.png";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 export const metadata: Metadata = {
-  // metadataBase: new URL(siteUrl),
+  metadataBase: new URL(siteUrl),
   title: '부착형 무선 데이터 서버',
   description: "제조 인프라의 혁신 부착형 무선 데이터 서버를 소개합니다.",
   keywords: "가격, 가격확인, 부착형 무선 데이터 서버, 무선 데이터 서버, 데이터 서버, 화낙, LAN, RS232, 스마트팩토리, 모니터링",
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     url: siteUrl,
     images: [
       {
-        url: `${Logopath}`,
+        url: Logopath.src,
         width: 1200,
         height: 630,
         alt: 'Betheone Logo',
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: '공작기계 솔루션 비더원',
     description: '가장 쉽고 빠른 부착형 데이터 서버',
-    images: [`${Logopath}`],
+    images: [Logopath.src],
   },
 }
 
