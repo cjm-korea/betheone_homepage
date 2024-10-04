a245ae2a145d54f6c94f5c55b6934a98acdb254c
CJM <whdals9514@naver.com>
2024-10-05 01:04:58 +0900

    :hammer: Naver SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   title: "공작기계 솔루션 비더원",
   description: "가장 쉽고 합리적인 제조 솔루션 공작기계 부착형 무선 데이터 서버, 캐드, 캠, 스마트팩토리, 모니터링 서비스를 소개합니다.",
   keywords: "제조 솔루션, 부착형 무선 데이터 서버, 화낙, LAN, RS232, 캐드, 캠, 스마트팩토리, 모니터링",
+  other: {
+    'naver-site-verification': 'ca865406870934095525f63f8b96c3e6cf7e6960'
+  }
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
