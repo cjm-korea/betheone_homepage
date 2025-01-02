a2eb95e57ddc45148f8ed2de9fd34516b759e0f9
cjm-korea <whdals9514@naver.com>
2025-01-03 02:56:32 +0900

    :hammer: Sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.smart-bethone.com',
+      url: 'https://www.smart-betheone.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://www.smart-bethone.com/and',
+      url: 'https://www.smart-betheone.com/and',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.smart-bethone.com/s2025',
+      url: 'https://www.smart-betheone.com/s2025',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
